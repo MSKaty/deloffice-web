cb366afa7386cc241b8199511a4b6167f9b3d7fb
VinceDelOffice <v.chu@printnpack.mu>
2020-06-25 16:10:14 +0400

    25.06.20 4:10pm

---
+++ src/app/common/services/productcategory.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+                
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductcategoryService {
+
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http:HttpClient) {}
+
+    public catfind(catId:string){
+      return this.http.get(this.apiUrl + '/category/' + catId);
+    }
+
+  }
+
--- src/app/views/product-list/product-list.component.ts
+++ src/app/views/product-list/product-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductcategoryService } from '../../common/services/productcategory.service';
 
+import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { TitleService } from '../../common/services/title.service';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  category$: Observable<any>;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _cat: ProductcategoryService,
+    private _title: TitleService) { }
 
   ngOnInit() {
+    this.category$ = this.get().pipe(
+      tap((category) => { this._title.changeTitle(category.des1) })
+    )
+  }
+  get() {
+    return this._route.params.pipe(
+      switchMap(param => {
+        return this._cat.catfind(param['id']);
+      })
+    )
   }
 
 }
